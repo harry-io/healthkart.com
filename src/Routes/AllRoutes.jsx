3bc13dbc472f63a899a1eadd12866eaab13e166c
bakshay96 <akshaymali.edu@gmail.com>
2023-02-22 19:51:43 +0530

    navbar added

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
 import Homepage from "../Pages/Homepage";
+import Search from "../Pages/Search/Search";
+
 
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Homepage />} />
+      {/* <Route path="/" element={<Homepage />} /> */}
+      <Route path="/" element={<Navbar />} />
+      <Route path="/Search" element={<Search />} />
     </Routes>
   );
 };
