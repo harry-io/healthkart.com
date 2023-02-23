fc7e26cf3ec3752643f834c32811f283fba2868f
bakshay96 <akshaymali.edu@gmail.com>
2023-02-23 22:33:00 +0530

    slider added

---
+++ src/Components/SliderImages/SliderImages.jsx
@@ -0,0 +1,48 @@
+export const MainBanner = [
+    {
+      srcLink: "https://img1.hkrtcdn.com/24436/bnr_2443520_o.jpg",
+    },
+    {
+      srcLink: "https://img1.hkrtcdn.com/24445/bnr_2444440_o.jpg",
+    },
+    {
+      srcLink: "https://img5.hkrtcdn.com/24429/bnr_2442834_o.jpg",
+    },
+    {
+      srcLink: "https://img3.hkrtcdn.com/24438/bnr_2443782_o.jpg",                            
+    },
+    {
+      srcLink: "https://img3.hkrtcdn.com/24441/bnr_2444072_o.jpg",
+    },
+    {
+      srcLink:"https://img3.hkrtcdn.com/24429/bnr_2442832_o.jpg",
+    }
+  ];
+  
+  export const midBanner = [
+    {
+      srcLink: "https://img1.hkrtcdn.com/22559/bnr_2255890_o.jpg",
+    },
+    {
+      srcLink: "https://img5.hkrtcdn.com/22743/bnr_2274254_o.jpg",
+    },
+    {
+      srcLink: "https://img9.hkrtcdn.com/22743/bnr_2274258_o.jpg",
+    },
+  ];
+  
+  export const lastBanner = [
+    {
+      srcLink: "https://img3.hkrtcdn.com/22874/bnr_2287312_o.jpg",
+    },
+    {
+      srcLink: "https://img5.hkrtcdn.com/22814/bnr_2281334_o.png",
+    },
+    {
+      srcLink: "https://img5.hkrtcdn.com/22781/bnr_2278004_o.png",
+    },
+    {
+      srcLink: "https://img3.hkrtcdn.com/22781/bnr_2278002_o.jpg",
+    },
+  ];
+  
