5867800c19a339d733e0b1d4535f199e5726e2f3
harry-io <112643733+harry-io@users.noreply.github.com>
2023-02-24 12:05:56 +0530

    updates in package.json

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -4,7 +4,7 @@ import Navbar from "../Components/Navbar/Navbar";
 import AddProduct from "../Pages/Admin/AddProduct";
 import Admin from "../Pages/Admin/Admin";
 import { EditProduct } from "../Pages/Admin/EditProduct";
-import Homepage from "../Pages/Homepage";
+import Homepage from "../Pages/Home/Homepage";
 import Login from "../Pages/Login/Login";
 import { Search } from "../Pages/Search/Search";
 import Signup from "../Pages/Signup/Signup";
@@ -12,7 +12,7 @@ import Signup from "../Pages/Signup/Signup";
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Homepage/>} />
+      <Route path="/" element={<Homepage />} />
       <Route path="/navbar" element={<Navbar />} />
       <Route path="/Search" element={<Search />} />
       <Route path="/login" element={<Login />} />
