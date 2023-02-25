08674b59bda616f8b60aab8f4bace63e1c13b777
gsandha <sandhagurpreet0@gmail.com>
2023-02-25 22:25:51 +0530

    Cart and payments

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,14 +1,29 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+
+import React, { useEffect, useState } from 'react';
 import styles from './ProductCard.module.css';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export const ProductCard = ({id,Title,image,price,stars,reviews}) => {
+  const [data,setData] = useState([]);
+  var carts = JSON.parse(localStorage.getItem('cart')) || [];
 
+  const handleCart = () => {
+    carts.push(data);
   
+    localStorage.setItem('cart',JSON.stringify(carts))
+    console.log(carts)
+}
+useEffect(()=>{
+  axios.get(`https://cute-gold-agouti-coat.cyclic.app/proteins/${id}`).then((res)=>{
+      setData(res.data)
+      console.log(res.data)
+  })
+},[])
   return (
     <div className={styles.main}>
         <div>
-          <Link to={`/single/${id}` }  >  
+          <Link to={`/product_list/single/${id}` }  >  
         <img src={image[0]} alt="" />
         </Link>
         </div>
@@ -20,7 +35,7 @@ export const ProductCard = ({id,Title,image,price,stars,reviews}) => {
             <h3>₹{price}</h3>
             
             <button className={styles.premium}>₹{price} for Premium Member</button>
-            <button className={styles.add}> Add to Cart</button>
+            <button className={styles.add} onClick={handleCart}> Add to Cart</button>
         </div>
     </div>
   )
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,9 +2,11 @@ import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
 import { adminReducer } from "./Admin/adminReducer";
 import { authReducer } from "./Auth/authReducer";
+import {reducer as ProductReducer} from "./Products/reducer"
 const rootReducer = {
   authReducer,
   adminReducer,
+ ProductReducer
 };
 
 export const store = legacy_createStore(
